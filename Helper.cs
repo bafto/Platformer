fc4823d503087c144f18b86aa6d716c4adaef553
TheLonelySheep <mr.sheep75@gmail.com>
2021-01-26 23:49:53 +0100

    added gravity, floaty movement, Main.deltaTime property, Main.screen property and various comments

---
+++ Helper.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Platformer
+{
+    static class Helper
+    {
+        // for extention or helper methods
+    }
+}
