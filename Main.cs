704cd69049c11a72bfe70c48f9b612f20065dcea
TheLonelySheep <mr.sheep75@gmail.com>
2021-01-26 18:29:58 +0100

    added mouse stuff auto maximize added screenwidth, screenHeight

--- Main.cs
+++ Main.cs
@@ -1,20 +1,39 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Diagnostics;
 
 namespace Platformer
 {
     public class Main : Game
     {
+        // game stuff
         public GraphicsDeviceManager graphics;
         public static SpriteBatch spriteBatch;
         public static Texture2D solid;
         public static SpriteFont font;
+        public static int screenWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
+        public static int screenHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
+        // mouse stuff
+        public static MouseState mouse = Mouse.GetState();
+        public static MouseState lastmouse;
+        public static bool LeftHeld;
+        public static bool RightHeld;
+        public static bool LeftReleased;
+        public static bool RightReleased;
+        public static bool LeftClick;
+        public static bool RightClick;
+        public static bool mouseMoved;
         public Main()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
+            Window.Title = "Platformer test";
             IsMouseVisible = true;
+            Window.AllowUserResizing = true;
+
+            System.Windows.Forms.Form form = (System.Windows.Forms.Form)System.Windows.Forms.Control.FromHandle(Window.Handle);
+            form.WindowState = System.Windows.Forms.FormWindowState.Maximized;
         }
 
         protected override void Initialize()
@@ -37,7 +56,7 @@ namespace Platformer
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+            UpdateMouse();
 
             base.Update(gameTime);
         }
@@ -53,6 +72,19 @@ namespace Platformer
             // TODO: Add your drawing code here
 
             base.Draw(gameTime);
+        }
+        private void UpdateMouse()
+        {
+            lastmouse = mouse;
+            mouse = Mouse.GetState();
+            mouseMoved = mouse.Position != lastmouse.Position;
+
+            LeftHeld = mouse.LeftButton == ButtonState.Pressed;
+            RightHeld = mouse.RightButton == ButtonState.Pressed;
+            LeftReleased = mouse.LeftButton == ButtonState.Released;
+            RightReleased = mouse.RightButton == ButtonState.Released;
+            LeftClick = LeftReleased && lastmouse.LeftButton == ButtonState.Pressed;
+            RightClick = RightReleased && lastmouse.RightButton == ButtonState.Pressed;
         }
     }
 }
