4cf461774dfde898381e9ef5a15dada54fb5fcda
TheLonelySheep <mr.sheep75@gmail.com>
2021-01-27 11:45:10 +0100

    Improved movement

--- Helper.cs
+++ Helper.cs
@@ -8,5 +8,17 @@ namespace Platformer
     static class Helper
     {
         // for extention or helper methods
+        public static bool isClamp(Vector2 value, Vector2 min, Vector2 max)
+        {
+            if (value.X < min.X || value.Y < min.Y)
+            {
+                return true;
+            }
+            if (value.X > max.X || value.Y > max.Y)
+            {
+                return true;
+            }
+            return false;
+        }
     }
 }
--- Player.cs
+++ Player.cs
@@ -18,26 +18,33 @@ namespace Platformer
         public Vector2 Velocity;
         private float moveTimer;
         public float acceleration;
+        public float drag;
+
         public Player()
         {
 
         }
         public void Initialize()
         {
-            position = new Vector2(0, 0);
+            position = new Vector2(100, 100);
             rect = new Rectangle(0, 0, 50, 50);
             color = Color.Red;
             maxSpeed = 10f;
             acceleration = 2f;
+            drag = 20;
         }
 
         public void Update()
         {
-            Velocity.Y += 0.1f; // gravity
-            if (Velocity.X != 0)
-                Velocity.X += 0.1f * -(Velocity.X / Math.Abs(Velocity.X)); //very bad slowdown sideways. needs improvement
-            moveTimer += 0.15f * Main.deltaTime; // increment timer. value represents how fast the player will reach maxSpeed
+            Velocity.Y += 5f * Main.deltaTime; // gravity
+            if (Velocity.X != 0) 
+            {
+                Velocity = Vector2.Lerp(Velocity, Vector2.Zero, moveTimer / drag); //very bad slowdown sideways. needs improvement
+            }
 
+            moveTimer += Main.deltaTime * 10; // increment timer. value represents how fast the player will reach maxSpeed
+
+            // Handle input
             if (Main.keyboard.IsKeyDown(Keys.A))
             {
                 Velocity.X -= MathHelper.Lerp(Velocity.X, maxSpeed, moveTimer) * acceleration * Main.deltaTime;
@@ -58,34 +65,20 @@ namespace Platformer
             {
                 moveTimer = 0;
             }
+
             // Clamp Velocity
             Velocity = Vector2.Clamp(Velocity, new Vector2(-maxSpeed), new Vector2(maxSpeed));
+
+            // Keep player in level bounds
+            position = Vector2.Clamp(position, Vector2.Zero, new Vector2(Main.screen.Width - 50, Main.screen.Height - 90));
+            if (Helper.isClamp(position, Vector2.Zero, new Vector2(Main.screen.Width - 50, Main.screen.Height - 90)))
+            {
+                Velocity = Vector2.Zero;
+            }
 
             // set position
             lastPosition = position;
             position += Velocity;
-
-            //Keeping Player on Screen. Removed later with greater World
-            if (position.X + rect.Width > Main.screen.Width)
-            {
-                position.X = Main.screen.Width - rect.Width;
-                Velocity.X = 0;
-            }
-            else if (position.X < 0)
-            {
-                position.X = 0;
-                Velocity.X = 0;
-            }
-            if (position.Y + rect.Height > Main.screen.Height)
-            {
-                position.Y = Main.screen.Height - rect.Height;
-                Velocity.Y = 0;
-            }
-            else if (position.Y < 0)
-            {
-                position.Y = 0;
-                Velocity.Y = 0;
-            }
         }
         public void Draw(SpriteBatch spriteBatch)
         {
