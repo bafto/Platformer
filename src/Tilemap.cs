fb6058e1cb8c66044dd0a8ae41a5e24c180f57c3
Hendrik Ziegler <hendrikziegler@outlook.de>
2021-02-22 10:05:49 +0100

    Commented the MakeHitboxes Function

--- src/Tilemap.cs
+++ src/Tilemap.cs
@@ -151,53 +151,55 @@ namespace Platformer.src
 
         private void MakeHitboxes()//Merge the tile rects together (Don't ask how it works. Please)
         {
+            //loop to each Tile
             for (int x = 0; x < width; x++)
             {
                 for (int y = 0; y < height; y++)
                 {
+                    //check if it is solid, and if it isn't already involved in a Hitbox
                     if (tiles[x, y].TileID != 0 && !tiles[x, y].inHitbox)
                     {
-                        RectangleF newHitbox = new RectangleF(tiles[x, y].rect);
-                        tiles[x, y].inHitbox = true;
-                        bool firstLoop = true, breaked = false;
-                        int xEnd = 0;
-                        for (int yy = y; yy < height; yy++)
+                        RectangleF newHitbox = new RectangleF(tiles[x, y].rect); //this will become the finished hitbox
+                        tiles[x, y].inHitbox = true; //the tile will definitely be in a hitbox, even if it is alone
+                        bool firstLoop = true, breaked = false; //temporary bools for later
+                        int xEnd = 0; //the X-Coordinate to determine where the Hitbox will end
+                        for (int yy = y; yy < height; yy++) //loop from top to bottom starting at the detected Tile (which isn't already in a Hitbox and is non-solid)
                         {
-                            int xx = x + 1;
-                            if (firstLoop)
+                            int xx = x + 1; //declared here to determine the width of the hitbox later
+                            if (firstLoop) //in the first loop, the width of the hitbox is determined so it starts one Tile to the right of the original one
                             {
-                                for (; xx < width && tiles[xx, yy].TileID != 0 && !tiles[xx, yy].inHitbox; xx++)
+                                for (; xx < width && tiles[xx, yy].TileID != 0 && !tiles[xx, yy].inHitbox; xx++) //loop from left to right until there is a air Tile
                                 {
-                                    newHitbox.Size.X += Tile.TileSize.X;
-                                    tiles[xx, yy].inHitbox = true;
+                                    newHitbox.Size.X += Tile.TileSize.X; //there is no air, so make the hitbox greater
+                                    tiles[xx, yy].inHitbox = true; //and set the added Tile to be in a hitbox
                                 }
-                                xEnd = xx;
+                                xEnd = xx; //the right end of the hitbox is set
                                 firstLoop = false;
                             }
                             else
                             {
-                                for (xx = x; xx < xEnd && tiles[xx, yy].TileID != 0 && !tiles[xx, yy].inHitbox; xx++)
+                                for (xx = x; xx < xEnd && tiles[xx, yy].TileID != 0 && !tiles[xx, yy].inHitbox; xx++) //we know the width, now we start at the tiles directly under the original one
                                 {
-                                    tiles[xx, yy].inHitbox = true;
+                                    tiles[xx, yy].inHitbox = true; //just add them to the hitbox, the width of it is already set correctly
                                 }
                             }
-                            if (xx != xEnd)
+                            if (xx != xEnd) //did we reach the full width of the hitbox?
                             {
-                                newHitbox.Size.Y -= Tile.TileSize.Y;
-                                breaked = true;
-                                for (int i = x; i < xx; i++)
+                                newHitbox.Size.Y -= Tile.TileSize.Y; //no, so we don't have to increase the height
+                                breaked = true; //we had to end cause it was no perfect rectangle anymore
+                                for (int i = x; i < xx; i++) // those hitboxes where added, but now we have to remove them
                                 {
                                     tiles[i, yy].inHitbox = false;
                                 }
                                 break;
                             }
-                            newHitbox.Size.Y += Tile.TileSize.Y;
+                            newHitbox.Size.Y += Tile.TileSize.Y; //we reached the width of the hitbox so we increase the height
                         }
                         if (!breaked)
                         {
-                            newHitbox.Size.Y -= Tile.TileSize.Y;
+                            newHitbox.Size.Y -= Tile.TileSize.Y; //it wasn't a perfect rectangle so we decrease the height
                         }
-                        hitboxes.Add(newHitbox);
+                        hitboxes.Add(newHitbox); //just add the Hitbox
                     }
                 }
             }
